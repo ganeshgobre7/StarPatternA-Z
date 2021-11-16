b06dd019fd0b090c67e86afb01ad3e07003ca054
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-11-16 11:34:06 +0530

    N_Pattern Program Added

--- StarPatternA_Z/Program.cs
+++ StarPatternA_Z/Program.cs
@@ -8,7 +8,7 @@ namespace StarPatternA_Z
         {
             Console.WriteLine("Welcome to the A-Z Star Pattern Program!!!");
             StarPattern pattern = new StarPattern();
-            pattern.Apattern();
+      /*      pattern.Apattern();
             pattern.Cpattern();
             pattern.Dpattern();
             pattern.Epattern();
@@ -19,7 +19,9 @@ namespace StarPatternA_Z
             pattern.Jpattern();
             pattern.Kpattern();
             pattern.Lpattern();
-            pattern.Mpattern();
+            pattern.Mpattern();*/
+            pattern.Npattern();
+
         }
     }
 }
--- StarPatternA_Z/StarPattern.cs
+++ StarPatternA_Z/StarPattern.cs
@@ -225,5 +225,24 @@ namespace StarPatternA_Z
                 Console.WriteLine();
             }
         }
+        public void Npattern()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 6; j++)
+                {
+                  if(j == 1 || j == 6 || i - j == 0)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+               
     }
 }
