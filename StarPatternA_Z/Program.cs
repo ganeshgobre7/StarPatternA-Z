333f5ea77568f24fd894681284ad74f1e61a6c28
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-11-16 22:05:57 +0530

    T_Pattern Program Added

--- StarPatternA_Z/Program.cs
+++ StarPatternA_Z/Program.cs
@@ -8,24 +8,25 @@ namespace StarPatternA_Z
         {
             Console.WriteLine("Welcome to the A-Z Star Pattern Program!!!");
             StarPattern pattern = new StarPattern();
-           /* pattern.Apattern();
-            pattern.Cpattern();
-            pattern.Dpattern();
-            pattern.Epattern();
-            pattern.Fpattern();
-            pattern.Gpattern();
-            pattern.Hpattern();
-            pattern.Ipattern();
-            pattern.Jpattern();
-            pattern.Kpattern();
-            pattern.Lpattern();
-            pattern.Mpattern();
-            pattern.Npattern();
-            pattern.Opattern();
-            pattern.Ppattern();
-            pattern.Qpattern();
-            pattern.Rpattern();*/
-            pattern.Spattern();
+            /* pattern.Apattern();
+             pattern.Cpattern();
+             pattern.Dpattern();
+             pattern.Epattern();
+             pattern.Fpattern();
+             pattern.Gpattern();
+             pattern.Hpattern();
+             pattern.Ipattern();
+             pattern.Jpattern();
+             pattern.Kpattern();
+             pattern.Lpattern();
+             pattern.Mpattern();
+             pattern.Npattern();
+             pattern.Opattern();
+             pattern.Ppattern();
+             pattern.Qpattern();
+             pattern.Rpattern();
+             pattern.Spattern();*/
+            pattern.Tpattern();
         }
     }
 }
--- StarPatternA_Z/StarPattern.cs
+++ StarPatternA_Z/StarPattern.cs
@@ -333,5 +333,23 @@ namespace StarPatternA_Z
                 Console.WriteLine();
             }
         }
+        public void Tpattern()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (i==1 || j==3)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
