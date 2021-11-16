5a09eccdf20c23e18f927f26e937b81fcaae859b
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-11-16 22:19:37 +0530

    U_Pattern Program Added

--- StarPatternA_Z/Program.cs
+++ StarPatternA_Z/Program.cs
@@ -8,25 +8,26 @@ namespace StarPatternA_Z
         {
             Console.WriteLine("Welcome to the A-Z Star Pattern Program!!!");
             StarPattern pattern = new StarPattern();
-            /* pattern.Apattern();
-             pattern.Cpattern();
-             pattern.Dpattern();
-             pattern.Epattern();
-             pattern.Fpattern();
-             pattern.Gpattern();
-             pattern.Hpattern();
-             pattern.Ipattern();
-             pattern.Jpattern();
-             pattern.Kpattern();
-             pattern.Lpattern();
-             pattern.Mpattern();
-             pattern.Npattern();
-             pattern.Opattern();
-             pattern.Ppattern();
-             pattern.Qpattern();
-             pattern.Rpattern();
-             pattern.Spattern();*/
-            pattern.Tpattern();
+/*            pattern.Apattern();
+            pattern.Cpattern();
+            pattern.Dpattern();
+            pattern.Epattern();
+            pattern.Fpattern();
+            pattern.Gpattern();
+            pattern.Hpattern();
+            pattern.Ipattern();
+            pattern.Jpattern();
+            pattern.Kpattern();
+            pattern.Lpattern();
+            pattern.Mpattern();
+            pattern.Npattern();
+            pattern.Opattern();
+            pattern.Ppattern();
+            pattern.Qpattern();
+            pattern.Rpattern();
+            pattern.Spattern();
+            pattern.Tpattern();*/
+            pattern.Upattern();
         }
     }
 }
--- StarPatternA_Z/StarPattern.cs
+++ StarPatternA_Z/StarPattern.cs
@@ -351,5 +351,23 @@ namespace StarPatternA_Z
                 Console.WriteLine();
             }
         }
+        public void Upattern()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    if ( j == 1 && i >= 2 && i <= 4 || j == 5 && i >= 2 && i <= 4 || i == 5 && j >= 2 && j <= 4 )
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
